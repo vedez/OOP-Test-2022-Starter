605b9a15aeb51b9e8d16e00ba9f8a604a66d260e
L Fernandez <72343133+vedez@users.noreply.github.com>
2022-04-27 16:42:35 +0100

    input data into array and set load & print func into settings

--- java/src/ie/tudublin/NematodeVisualiser.java
+++ java/src/ie/tudublin/NematodeVisualiser.java
@@ -8,6 +8,8 @@ import processing.data.TableRow;
 
 public class NematodeVisualiser extends PApplet
 {
+	//Create an array list for Nematodes
+	public ArrayList<Nematode> nematodes = new ArrayList<Nematode>();
 
 	public void keyPressed()
 	{		
@@ -19,7 +21,12 @@ public class NematodeVisualiser extends PApplet
 
 	public void settings()
 	{
+		//Create size of application
 		size(800, 800);
+
+		//Call load and print functions
+		loadNematodes();
+		printNematodes();
 	}
 
 	public void setup() 
@@ -28,11 +35,23 @@ public class NematodeVisualiser extends PApplet
 		background(0);
 		smooth();				
 	}
-	
 
-	public void loadNematodes()
-	{
+	//loads and input data into the array list 
+	public void loadNematodes() {
+		Table t = loadTable("nematodes.csv", "header");
+
+		for(TableRow row:t.rows()) {
+			Nematode n = new Nematode(row);
+			nematodes.add(n);
+		}
 	}
+
+	public void printNematodes() {
+		for(Nematode n:nematodes) {
+			System.out.println(n);
+		}
+	}
+	
 
 
 	public void draw()
